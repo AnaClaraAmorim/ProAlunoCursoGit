7e2b12078a73da238c163ccbaed9832d2542acfb
DESKTOP-F9GHF2T\T-Gamer <anaclaramorim2014@gmail.com>
2020-11-19 03:00:15 -0300

    adicionando funcao

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void function(){
+    int x;
+    scanf("%d",&x);
+    printf("%d",x);
+}
 int main(){
+    function();
     printf("Hello World");
     printf("Hello World2");
 }
